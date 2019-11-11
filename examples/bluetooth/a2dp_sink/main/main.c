add357cb155714f0eb45a663a79be968e8a5bc1d
Herschdorfer <herschdorfer@gmail.com>
2019-11-11 18:44:45 +0100

    - fix missing include

--- examples/bluetooth/a2dp_sink/main/main.c
+++ examples/bluetooth/a2dp_sink/main/main.c
@@ -12,6 +12,8 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+#include "sdkconfig.h"
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
